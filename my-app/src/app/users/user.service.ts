c7bfcb677138ff155f8550cbb1052d21f5c1e7e5
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-07-29 11:51:02 +0200

    Users List

--- my-app/src/app/users/user.service.ts
+++ my-app/src/app/users/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ export class UserService {
   constructor(private httpClient: HttpClient) { }
 
   getAll() {
-
+    // const req = new HttpRequest('GET', 'https://jsonplaceholder.typicode.com/users', {
+    //   headers: {}
+    // });
+    // this.httpClient.request(req)
+    return this.httpClient.get<User[]>('https://jsonplaceholder.typicode.com/users')
   }
 }
--- my-app/src/app/users/users/users.component.ts
+++ my-app/src/app/users/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
 import { UserService } from '../user.service';
 
 @Component({
@@ -7,10 +8,14 @@ import { UserService } from '../user.service';
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+
+  users: User[] = [];
+
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    console.log(this.userService);
-    this.userService.getAll();
+    this.userService.getAll().subscribe((data) => {
+      this.users = data;
+    });
   }
 }
